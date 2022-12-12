5d327ac878a10eca6fa79b44561d86402f8b6893
Diksha Prasad <68725958+DIKBIT@users.noreply.github.com>
2022-12-12 12:47:58 +0530

    basic UI done before validation of the form

--- yogaloginform/src/components/Form.js
+++ yogaloginform/src/components/Form.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import bgImg from '../assets/3921140.jpg'
 
 const Form = () => {
   return (
-    <div>Form</div>
+    <section>
+        <div className = 'register'>
+        <div className='col-1'>
+        <h2>YOGA SIGN IN FORM</h2>
+        <span>Stay calm and fit </span>
+        
+        <form id='form' className='flex flex-col'>
+        <input type="text" placeholder='username'/>
+        <input type="text" placeholder='password'/>
+        <input type="text" placeholder='confirm password'/>
+        <input type="text" placeholder='mobile no.'/>
+        <input type="text" placeholder='date of birth'/>
+
+
+        <button className='btn'>SIGN IN </button>
+
+        </form>
+        </div>
+        <div className='col-2'>
+            <img src={bgImg} alt="yoga image"></img>
+        </div>
+
+        </div>
+    </section>
   )
 }
 
