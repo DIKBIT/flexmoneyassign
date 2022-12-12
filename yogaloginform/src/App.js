6764faefa70d012c07b26ac7da3e8e2ee83536ab
Diksha Prasad <68725958+DIKBIT@users.noreply.github.com>
2022-12-12 19:08:32 +0530

    routing between login and register form done, home page left

--- yogaloginform/src/App.js
+++ yogaloginform/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 import Form from './components/Form';
-
+import Register from './components/Register';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-    <Form></Form>
+    <BrowserRouter>
+      <Routes>
+      <Route path='/login' element={<Form/>}></Route>
+      <Route path='/register' element={<Register/>}></Route>
+      </Routes>
+    </BrowserRouter>
+
+
+
+
+    {/* <Form>
+      <Register></Register>
+    </Form> */}
+    
       
     </div>
   );
